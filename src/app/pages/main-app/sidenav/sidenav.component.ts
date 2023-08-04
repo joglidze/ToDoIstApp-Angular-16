bae8cb03ab8aa9af38edc5f7d0602b77317f6cdb
luca <lucajoglidze@gmail.com>
2023-08-04 08:13:32 -0700

    add routing and other children component routes

--- src/app/pages/main-app/sidenav/sidenav.component.ts
+++ src/app/pages/main-app/sidenav/sidenav.component.ts
@@ -2,10 +2,11 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { NavDrawerComponent } from './nav-drawer/nav-drawer.component';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [CommonModule, MatSidenavModule, NavDrawerComponent],
+  imports: [CommonModule, MatSidenavModule, NavDrawerComponent,RouterOutlet],
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
 })
