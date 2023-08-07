79e6614763378a90f834c84a974201fc8eb1e52d
luca <lucajoglidze@gmail.com>
2023-08-06 18:38:26 -0700

    create task components and display task

--- src/app/components/inbox/inbox.component.ts
+++ src/app/components/inbox/inbox.component.ts
@@ -1,29 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateTaskComponent } from '../create-task/create-task.component';
 import { TaskService } from 'src/app/core/services/task.service';
 import { LocalstorageService } from 'src/app/facade/localstorage.service';
+import { TaskComponent } from '../task/task.component';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-inbox',
   standalone: true,
-  imports: [CommonModule, CreateTaskComponent],
+  imports: [CommonModule, CreateTaskComponent, TaskComponent],
   templateUrl: './inbox.component.html',
   styleUrls: ['./inbox.component.scss'],
 })
-export class InboxComponent implements OnInit{
+export class InboxComponent implements OnInit {
   taskTrigger: boolean = false;
-  
-  
-  constructor(private taskService:TaskService,private localService:LocalstorageService){}
+  taskArray: any = [];
+
+  constructor(
+    private taskService: TaskService,
+    private localService: LocalstorageService
+  ) {}
   ngOnInit(): void {
-    this.taskService
-    .get(`${this.localService.localUser()}/inbox.json`,)
-    .subscribe((res) => {
-      console.log(res);
-    });
+    this.getTasks();
   }
-  
+  getTasks() {
+    this.taskService
+      .get(`${this.localService.localUser()}/inbox.json`)
+      .pipe(map((res: any) => Object.values(res)))
+      .subscribe((res: any) => {
+        console.log(res);
+        this.taskArray = [...res];
+        console.log(this.taskArray);
+      });
+  }
+
   closeTask(test: any) {
     this.taskTrigger = test;
   }
